ce014000e6baf47e0143f318acdfe41227095546
Sreekar738 <sreekarreddy.cs24@bmsce.ac.in>
2025-11-18 08:59:46 +0530

    Create singly linked list delete.c

---
+++ lab 6/singly linked list delete.c
@@ -0,0 +1,200 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* head = NULL;
+
+void insertEnd(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    struct Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void insertBeginning(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = head;
+    head = newNode;
+}
+
+void insertAtPosition(int value, int pos) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (pos == 1) {
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+
+    struct Node* temp = head;
+    int i;
+    for (i = 1; i < pos - 1 && temp != NULL; i++) {
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Position out of range.\n");
+        free(newNode);
+        return;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+void deleteFirst() {
+    if (head == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
+    struct Node* temp = head;
+    head = head->next;
+    free(temp);
+}
+
+void deleteLast() {
+    if (head == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
+    if (head->next == NULL) {
+        free(head);
+        head = NULL;
+        return;
+    }
+
+    struct Node* temp = head;
+    while (temp->next->next != NULL) {
+        temp = temp->next;
+    }
+
+    free(temp->next);
+    temp->next = NULL;
+}
+
+void deleteSpecified(int value) {
+    if (head == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
+
+    struct Node* temp = head;
+    struct Node* prev = NULL;
+
+    if (temp->data == value) {
+        head = temp->next;
+        free(temp);
+        return;
+    }
+
+    while (temp != NULL && temp->data != value) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Element %d not found.\n", value);
+        return;
+    }
+
+    prev->next = temp->next;
+    free(temp);
+}
+
+void display() {
+    if (head == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
+
+    struct Node* temp = head;
+    printf("Linked List: ");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    int choice, value, pos;
+
+    while (1) {
+        printf("\n--- MENU ---\n");
+        printf("1. Insert at End\n");
+        printf("2. Insert at Beginning\n");
+        printf("3. Insert at Position\n");
+        printf("4. Delete First\n");
+        printf("5. Delete Specified Element\n");
+        printf("6. Delete Last\n");
+        printf("7. Display\n");
+        printf("8. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+        case 1:
+            printf("Enter value: ");
+            scanf("%d", &value);
+            insertEnd(value);
+            break;
+
+        case 2:
+            printf("Enter value: ");
+            scanf("%d", &value);
+            insertBeginning(value);
+            break;
+
+        case 3:
+            printf("Enter value: ");
+            scanf("%d", &value);
+            printf("Enter position: ");
+            scanf("%d", &pos);
+            insertAtPosition(value, pos);
+            break;
+
+        case 4:
+            deleteFirst();
+            break;
+
+        case 5:
+            printf("Enter element to delete: ");
+            scanf("%d", &value);
+            deleteSpecified(value);
+            break;
+
+        case 6:
+            deleteLast();
+            break;
+
+        case 7:
+            display();
+            break;
+
+        case 8:
+            exit(0);
+
+        default:
+            printf("Invalid choice.\n");
+        }
+    }
+
+    return 0;
+}
